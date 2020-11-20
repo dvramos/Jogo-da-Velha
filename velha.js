69c64feb5aaf28624f65d50ab2b09799d182b6fd
dvramos <dvramos@hotmail.com.br>
2020-11-20 17:09:40 -0300

    DIO 8/8
    Finish

--- velha.js
+++ velha.js
@@ -7,6 +7,10 @@ mudarJogador('X');
 
 function escolherQuadrado(id){
 
+    if (vencedor !== null) {
+        return;
+    }
+
     var quadrado = document.getElementById(id);
     if (quadrado.innerHTML !== '-'){
         return;
@@ -62,7 +66,22 @@ function checaVencedor(){
     }
     if (checaSequencia (quadrado3, quadrado5, quadrado7)){
         mudaCorQuadrado(quadrado3, quadrado5, quadrado7);
-        mudarVencedor(quadrado4);
+        mudarVencedor(quadrado3);
+        return;
+    }
+    if (checaSequencia (quadrado1, quadrado4, quadrado7)){
+        mudaCorQuadrado(quadrado1, quadrado4, quadrado7);
+        mudarVencedor(quadrado1);
+        return;
+    }
+    if (checaSequencia (quadrado2, quadrado5, quadrado8)){
+        mudaCorQuadrado(quadrado2, quadrado5, quadrado8);
+        mudarVencedor(quadrado2);
+        return;
+    }
+    if (checaSequencia (quadrado3, quadrado6, quadrado9)){
+        mudaCorQuadrado(quadrado3, quadrado6, quadrado9);
+        mudarVencedor(quadrado3);
         return;
     }
 }
@@ -86,4 +105,17 @@ function checaSequencia(quadrado1,quadrado2,quadrado3,){
         eigual = true;
     }
     return eigual;
+}
+
+function reiniciar(){
+    vencedor = null;
+    vencedorSelecionado.innerHTML = '';
+
+    for (var i=1; i<=9; i++){
+        var quadrado = document.getElementById(i);
+        quadrado.style.background = '#eee';
+        quadrado.style.color = '#eee';
+        quadrado.innerHTML = '-';
+    }
+    mudarJogador('X')
 }
